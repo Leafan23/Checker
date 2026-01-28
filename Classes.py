a4f1d28febf575201a9ada7dcdcaa90d511d0b50
Leafan <la2player_07@mail.ru>
2026-01-29 00:06:13 +0300

    v0.1 (Build 20.2)
    TODO дописать функцию add_file, для дальнейшего присвоения свойств объектов

--- Classes.py
+++ Classes.py
@@ -3,7 +3,7 @@ from typing import Any, Self
 
 
 class File:
-    def __init__(self, kompas: str | Any, id=0):
+    def __init__(self, kompas: str | Any):
         self.kompas = kompas
         self.id = None
         if isinstance(kompas, str):
@@ -23,25 +23,31 @@ class File:
 
 
 class Pdf(File):
-    def __init__(self, kompas, id_number):
-        super().__init__(kompas, id_number)
+    def __init__(self, kompas):
+        super().__init__(kompas)
         self.type = 1
 
 
 class Part(File):
-    def __init__(self, kompas, id_number):
-        super().__init__(kompas, id_number)
+    def __init__(self, kompas):
+        super().__init__(kompas)
         self.type = 2
         self.drawing = False # чертеж присутствует - True; чертеж отсутствует - False
-        self.drawing_number = kompas.drawing_number
-        self.drawing_name = kompas.drawing_name
+        self.drawing_number = ''
+        self.drawing_name = ''
 
 
 class Assemble(Part):
-    def __init__(self, kompas, id_number):
-        super().__init__(kompas, id_number)
+    def __init__(self, kompas):
+        super().__init__(kompas)
         self.type = 3
         self.bill_of_material = False # спецификация присутствует - True; спецификация отсутствует - False
+
+
+class Drawing(File):
+    def __init__(self, kompas):
+        super().__init__(kompas)
+        self.type = 4
 
 
 class Files:
@@ -55,23 +61,30 @@ class Files:
 
         self._id_count = 0
 
-    def add_file(self, file: str):
+    def add_file(self, file: str): #TODO дописать для заполнения всех элементов
         """Принимает строку со ссылкой на файл"""
         self._id_count += 1
-        file_object = File(file)
+        if os.path.splitext(file)[1] == '.a3d': file_object = Assemble(file)
+        elif os.path.splitext(file)[1] == '.m3d': file_object = Part(file)
+        elif os.path.splitext(file)[1] == '.cdw': file_object = Drawing(file)
+        else: file_object = File(file)
         file_object.id = self._id_count
 
         self.all_objects.append(file_object)
-        if file_object.type == 2:
-            self.m3d_list.append(file_object)
-        if file_object.type == 3:
-            self.a3d_list.append(file_object)
+        if file_object.type == 2: self.m3d_list.append(file_object)
+        elif file_object.type == 3: self.a3d_list.append(file_object)
+        elif file_object.type == 4: self.cdw_list.append(file_object)
 
-    def find_missing_drawing(self):
+    def find_missing_drawing(self): #TODO Переписать под изменения
+        list_to_check = []
 
+        for i in self.cdw_list:
+            list_to_check.append(os.path.splitext(i.path)[0])
+        print(self.cdw_list)
+        print(list_to_check)
         for i in self.a3d_list:
-
-            if i.path in self.cdw_list:
+            print(os.path.splitext(i.path)[0])
+            if os.path.splitext(i.path)[0] in self.cdw_list:
                 i.drawing = True
 
     def id_return(self, id: int):
@@ -85,3 +98,5 @@ class Files:
     def print_all_data(self):
         for i in self.all_objects:
             print('id: ', i.id, '   path: ',i.path, '   parent: ',i.parent, '   child: ',i.child)
+            if isinstance(i, Part): print('   drawing: ', i.drawing)
+            if isinstance(i, Assemble): print('   drawing: ', i.drawing,'   BOM: ',i.bill_of_material)
--- main.py
+++ main.py
@@ -9,18 +9,8 @@ import API
 
 
 if __name__ == '__main__':
-    #попросить компас вернуть текущий документ
     kompasAPI = API.API()
     kompasAPI.scan(r"C:\Users\Leafan\PycharmProjects\Checker\kompas_files\ГКЮШ.ТЕСТ.00.000.a3d")
-    #kompasAPI.open(r"D:\Projects\Checker\Kompas_files\ГКЮШ.ТЕСТ.00.000.a3d")
-    #print(kompasAPI.main_tree[0].path)
-    '''for i in kompasAPI.main_tree:
-        print('Drawing: ', i.drawing, i.path)
-        try:
-            print('BOM: ', i.bill_of_material, i.path)
-        except:
-            pass
-        print('-----------------------')'''
-
+    kompasAPI.files.find_missing_drawing()
     kompasAPI.files.print_all_data()
     #TODO полное закрытие приложения, если работало в скрытом режиме
